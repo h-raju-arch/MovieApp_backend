58761e034d39f4088d0ebb6b86c50dbd5d0acf2c
h-raju-arch <h-raju@mercari.com>
2026-01-30 16:14:09 +0530

    better error handling in  seed.go

--- seed.go
+++ seed.go
@@ -32,7 +32,13 @@ type execer interface {
 
 func main() {
 	database := db.Open()
-	defer database.Close()
+
+	defer func(database *sql.DB) {
+		err := database.Close()
+		if err != nil {
+			log.Fatalf("err: %v", err)
+		}
+	}(database)
 
 	ctx := context.Background()
 	log.Println("seeding database..")
